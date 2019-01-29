1a0c44147bf75fd2dabc6940472e9cb6dd4c9c64
MedMath <medmathandit@gmail.com>
2019-01-29 10:23:51 -0500

    added TestBase Driver BrowserUtils ConfigurationReader

--- src/test/java/com/lunch/utilities/TestBase.java
+++ src/test/java/com/lunch/utilities/TestBase.java
@@ -1,4 +1,51 @@
 package com.lunch.utilities;
 
-public class TestBase {
-}
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public abstract class TestBase {
+    protected WebDriver driver;
+    protected Pages pages;
+
+    protected static ExtentReports report;
+    protected static ExtentHtmlReporter htmlReporter;
+    protected static ExtentTest extentLogger;
+
+    @BeforeMethod
+    public void setupMethod() {
+        driver = Driver.getDriver();
+        pages = new Pages();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(ConfigurationReader.getProperty("url"));
+    }
+
+    @AfterMethod
+    public void tearDownMethod(ITestResult result) throws IOException {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            String screenshotLocation = BrowserUtils.getScreenshot(result.getName());
+            extentLogger.fail(result.getName());
+            extentLogger.addScreenCaptureFromPath(screenshotLocation);
+            extentLogger.fail(result.getThrowable());
+
+        } else if (result.getStatus() == ITestResult.SKIP) {
+            extentLogger.skip("Test Case Skipped: " + result.getName());
+        }
+        Driver.closeDriver();
+    }
+
+    @BeforeTest
+    public void setUpTest() {
+        report = new ExtentReports();
+        String filePath = System.getProperty("user.dir") + "/test-output/report.html";
+        htmlReporter = new ExtentHtmlReporter(filePath);
+
+        report.attachReporter(htmlReporter);
