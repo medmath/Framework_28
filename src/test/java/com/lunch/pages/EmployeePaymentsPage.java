2a4076a302af80cc0503e1e0b2d178420c65761e
Bilgehan16 <bsag16@gmail.com>
2019-02-01 14:08:29 -0500

    add ExportCVS test case

--- src/test/java/com/lunch/pages/EmployeePaymentsPage.java
+++ src/test/java/com/lunch/pages/EmployeePaymentsPage.java
@@ -20,6 +20,26 @@ public class EmployeePaymentsPage {
     @FindBy(css = ".o_form_buttons_edit [type='button']:nth-of-type(1)")
     public WebElement save;
 
+    @FindBy(css = ".ui-sortable .o_data_row:nth-of-type(10) [type]")
+    public WebElement newCheckBox;
+    @FindBy(css = ".o_cp_sidebar .o_dropdown:nth-of-type(2) [data-toggle]")
+    public WebElement actionButton;
+    @FindBy(css=".o_cp_sidebar li:nth-of-type(1) [href]")
+    public WebElement newExport;
+    @FindBy(css=".o_field_tree_structure [tabindex='-1']:nth-of-type(10) .o_tree_column")
+    public WebElement newUser;
+    @FindBy(css=".o_field_tree_structure [tabindex='-1']:nth-of-type(4) .o_tree_column")
+    public WebElement newDate;
+    @FindBy(css = ".o_field_tree_structure [tabindex='-1']:nth-of-type(1) .o_tree_column")
+    public WebElement newExportAmount;
+    @FindBy(css=".o_center_panel [type='button']:nth-of-type(1)")
+    public WebElement newAdd;
+    @FindBy(css = ".o_center_panel [type='button']:nth-of-type(3)")
+    public WebElement newRemoveAll;
+
+
+
+
     public void clickCreate() {
         createButton.click();
     }
@@ -37,4 +57,30 @@ public class EmployeePaymentsPage {
         save.click();
     }
 
+    public void selectCheckBox(){
+        newCheckBox.click();
+    }
+
+    public void clickActionButton(){
+        actionButton.click();
+    }
+    public void clickExport(){
+        newExport.click();
+    }
+    public void clickNewUser(){
+        newUser.click();
+        newAdd.click();
+    }
+    public void clickNewDate(){
+        newDate.click();
+        newAdd.click();
+    }
+    public void clickNewExportAmount(){
+        newExportAmount.click();
+        newAdd.click();
+    }
+
+    public void clickNewRemoveAll(){
+        newRemoveAll.click();
+    }
 }
