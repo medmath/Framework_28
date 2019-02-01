563f237be9f1b6f1555ae938665eacf856445f87
Bilgehan16 <bsag16@gmail.com>
2019-02-01 15:37:07 -0500

    add Delete register cash

---
+++ src/test/java/com/lunch/tests/functional_tests/lunch/employeePaymentTab/deleteRegisterCash.java
@@ -0,0 +1,40 @@
+package com.lunch.tests.functional_tests.lunch.employeePaymentTab;
+
+import com.lunch.utilities.ConfigurationReader;
+import com.lunch.utilities.TestBase;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class deleteRegisterCash extends TestBase {
+    @Test
+    public void exportCvs() {
+        extentLogger = report.createTest("Export already created register cash moves.");
+        pages.login().login(ConfigurationReader.getProperty("mailManager"), ConfigurationReader.getProperty("passwordManager"));
+
+        extentLogger.info("Going to lunch page");
+        pages.login().clickLunchButton();
+
+        extentLogger.info("Getting past error");
+        pages.lunchHomePage().clickOkError();
+
+        extentLogger.info("Going to Employee Payments");
+        pages.lunchHomePage().employeePaymentsButton.click();
+
+        extentLogger.info("Going to Employee Payments");
+        pages.lunchHomePage().employeePaymentsButton.click();
+
+        extentLogger.info("Select checkbox on 01/21/2019");
+        pages.getEmployeePaymentsPage().selectCheckBox();
+
+        extentLogger.info("Going to Action");
+        pages.getEmployeePaymentsPage().clickActionButton();
+
+        extentLogger.info("Going to delete");
+        pages.getEmployeePaymentsPage().clickDelete();
+
+        extentLogger.info("Clicking Ok");
+        driver.findElement(By.cssSelector(".modal-footer [type='button']:nth-of-type(1) span"));
+
+
+    }
+}
