c5e65d285b355c8e14ee28f4477f180866543b40
MedMath <medmathandit@gmail.com>
2019-02-04 14:36:55 -0500

    added alert export functionalty test

--- src/test/java/com/lunch/pages/AlertsPage.java
+++ src/test/java/com/lunch/pages/AlertsPage.java
@@ -17,6 +17,24 @@ public class AlertsPage {
     @FindBy(css = ".ui-sortable .o_data_row:nth-of-type(1) [type]")
     public WebElement firstCheckButton;
 
+    @FindBy(css = ".o_cp_sidebar .o_dropdown:nth-of-type(2) [data-toggle]")
+    public WebElement ActionButton;
+
+    @FindBy(css = ".o_cp_sidebar li:nth-of-type(1) [href]")
+    public WebElement exportButton;
+
+    @FindBy(css = ".o_field_tree_structure [tabindex='-1']:nth-of-type(1) .o_tree_column")
+    public WebElement activeField;
+    
+    @FindBy(css = ".o_center_panel [type='button']:nth-of-type(1)")
+    public WebElement addField;
+
+    @FindBy(css = ".modal-footer [type='button']:nth-of-type(1) span")
+    public WebElement exportToFileButton;
+
+    @FindBy(css = ".modal-footer [type='button']:nth-of-type(2) span")
+    public WebElement closeButton;
+
     public boolean checkAllSelected(){
         for (int i = 1; i <=5 ; i++) {
             if(!Driver.getDriver().findElement(By.cssSelector(".ui-sortable .o_data_row:nth-of-type("+i+") [type]")).isSelected())
@@ -24,6 +42,14 @@ public class AlertsPage {
 
         }
         return true;
+    }
+
+    public void setAddField(){
+        for (int i = 1; i < 10; i++) {
+            Driver.getDriver().findElement(By.cssSelector(".o_field_tree_structure [tabindex='-1']:nth-of-type("+i+") .o_tree_column")).click();
+            addField.click();
+            
+        }
     }
 
 }
--- src/test/java/com/lunch/tests/functional_tests/lunch/AlertsTest.java
+++ src/test/java/com/lunch/tests/functional_tests/lunch/AlertsTest.java
@@ -1,5 +1,6 @@
 package com.lunch.tests.functional_tests.lunch;
 
+import com.lunch.utilities.BrowserUtils;
 import com.lunch.utilities.ConfigurationReader;
 import com.lunch.utilities.Driver;
 import com.lunch.utilities.TestBase;
@@ -37,4 +38,47 @@ public class AlertsTest extends TestBase {
 
 
     }
+
+    @Test
+    public void exportFunctionality()  {
+        extentLogger = report.createTest("Alerts Page check list check ");
+
+        extentLogger.info("Logging to the application");
+        pages.login().login(ConfigurationReader.getProperty("mailManager"), ConfigurationReader.getProperty("passwordManager"));
+
+        extentLogger.info("Going to lunch page");
+        pages.login().clickLunchButton();
+
+//        extentLogger.info("Getting past error");
+//        pages.lunchHomePage().clickOkError();
+
+        extentLogger.info("Going to Alerts section.");
+        pages.lunchHomePage().alertsPageButton.click();
+
+        extentLogger.info("checking all buttons");
+        pages.getAlertsPage().checkListAllButton.click();
+
+        Driver.getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        extentLogger.info("export");
+        pages.getAlertsPage().ActionButton.click();
+        pages.getAlertsPage().exportButton.click();
+
+        pages.getAlertsPage().activeField.click();
+        pages.getAlertsPage().addField.click();
+        pages.getAlertsPage().setAddField();
+
+
+
+        extentLogger.info("Exporting to file");
+        pages.getAlertsPage().exportToFileButton.click();
+
+        extentLogger.info("Closing the export section");
+
+        extentLogger.pass("Succesfully all checked");
+
+
+    }
+
+
 }
