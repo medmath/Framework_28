9f45e4fb659cb770efa7af27d7646bea98449ede
MedMath <medmathandit@gmail.com>
2019-02-04 13:57:44 -0500

    added alert check all test

--- src/test/java/com/lunch/pages/AlertsPage.java
+++ src/test/java/com/lunch/pages/AlertsPage.java
@@ -1,6 +1,7 @@
 package com.lunch.pages;
 
 import com.lunch.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -10,9 +11,19 @@ public class AlertsPage {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
-    @FindBy(css = "[data-menu='107']")
+    @FindBy(css = "thead [type]")
     public WebElement checkListAllButton;
 
+    @FindBy(css = ".ui-sortable .o_data_row:nth-of-type(1) [type]")
+    public WebElement firstCheckButton;
 
+    public boolean checkAllSelected(){
+        for (int i = 1; i <=5 ; i++) {
+            if(!Driver.getDriver().findElement(By.cssSelector(".ui-sortable .o_data_row:nth-of-type("+i+") [type]")).isSelected())
+                return false;
+
+        }
+        return true;
+    }
 
 }
--- src/test/java/com/lunch/tests/functional_tests/lunch/AlertsTest.java
+++ src/test/java/com/lunch/tests/functional_tests/lunch/AlertsTest.java
@@ -1,12 +1,16 @@
 package com.lunch.tests.functional_tests.lunch;
 
 import com.lunch.utilities.ConfigurationReader;
+import com.lunch.utilities.Driver;
 import com.lunch.utilities.TestBase;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class AlertsTest extends TestBase {
     @Test
-    public void checkAllButton(){
+    public void checkAllButton() throws InterruptedException {
         extentLogger = report.createTest("Alerts Page check list check ");
 
         extentLogger.info("Logging to the application");
@@ -21,7 +25,16 @@ public class AlertsTest extends TestBase {
         extentLogger.info("Going to Alerts section.");
         pages.lunchHomePage().alertsPageButton.click();
 
-
+        extentLogger.info("checking all buttons");
         pages.getAlertsPage().checkListAllButton.click();
+
+        Driver.getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        extentLogger.info("Verifying all checklists selected");
+        Assert.assertEquals(pages.getAlertsPage().checkAllSelected(),true);
+
+        extentLogger.pass("Succesfully all checked");
+
+
     }
 }
