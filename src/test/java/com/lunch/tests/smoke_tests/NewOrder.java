4a23c14d33e66e9f5039481f80594e8cf124e341
Roger <rogerk1591@gmail.com>
2019-02-07 22:22:25 -0500

    Made changse to new order page. Added smoke test "New Order"
    New Order allows test to create a new order, then delete it immediately
    after so that the previous orders page is not cluttered.

---
+++ src/test/java/com/lunch/tests/smoke_tests/NewOrder.java
@@ -0,0 +1,47 @@
+package com.lunch.tests.smoke_tests;
+
+import com.lunch.utilities.ConfigurationReader;
+import com.lunch.utilities.TestBase;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewOrder extends TestBase {
+
+    @Test
+    public void NewOrderTest(){
+        extentLogger = report.createTest("New Order Smoke Test");
+
+        extentLogger.info("Signing in..");
+        pages.login().login(ConfigurationReader.getProperty("mailManager"), ConfigurationReader.getProperty("passwordManager"));
+
+
+        extentLogger.info("Going to lunch page");
+        pages.login().clickLunchButton();
+
+        extentLogger.info("Ordering CheeseBurger");
+        pages.getNewOrderPage().clickAddCheeseburger();
+
+        String beforeOrder = driver.findElement(By.cssSelector(".breadcrumb li")).getText();
+
+
+        extentLogger.info("Saving order..");
+        pages.getNewOrderPage().clickSave();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        String afterOrder =  driver.findElement(By.cssSelector(".breadcrumb li")).getText();
+
+       Assert.assertNotEquals(beforeOrder, afterOrder);
+
+        extentLogger.pass("Passed Positive New Order Test");
+
+        pages.getNewOrderPage().deleteJustMadeItem();
+
+    }
+
+}
